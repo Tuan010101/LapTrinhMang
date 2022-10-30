9b1357ca2b53684e1aa6f1e34f5411fe9e9e2983
Tuan010101 <90946207+Tuan010101@users.noreply.github.com>
2022-10-31 00:05:42 +0700

    up bt file

---
+++ file/SapXepVaTinhTong.java
@@ -0,0 +1,37 @@
+package file;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class SapXepVaTinhTong {
+    public static ArrayList<Float> sort(String input) {
+        StringTokenizer stringTokenizer = new StringTokenizer(input," ");
+        float tong = 0;
+        ArrayList<Float> arrayList = new ArrayList<Float>();
+        while (stringTokenizer.hasMoreTokens()){
+            String token = stringTokenizer.nextToken();
+            Float value = Float.parseFloat(token);
+            arrayList.add(value);
+            tong += value;
+        }
+        Collections.sort(arrayList);
+        try {
+            FileWriter output = new FileWriter("file/output.txt");
+            output.write("Day so tang dan: " + arrayList.toString().substring(1, arrayList.toString().length()-1) + "\n");
+            output.write("Tong cac so: " + tong);
+            System.out.println("Day so tang dan: " + arrayList.toString().substring(1, arrayList.toString().length()-1));
+            System.out.println("Day so tang dan: " + arrayList);
+            System.out.println("Tong cac so: " + tong);
+            output.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+    public static void main(String[]ags) throws IOException {
+        String input = new String(Files.readAllBytes(Paths.get("file/input.txt")));
+        sort(input);
+    }
+}
